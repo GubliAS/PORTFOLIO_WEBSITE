3d2caeec6017d131653e1a9e15a2c237b8da1b99
GubliAS <159706365+GubliAS@users.noreply.github.com>
2025-08-29 16:44:28 +0000

    Update ContainerBlock.js

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -10,7 +10,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const meta = {
     title: "Abdullai Gubli Sitobu - Frontend Developer, UI/UX Designer",
     description: `I've been developing websites for 2 years straight. Get in touch with me to know more.`,
-    image: "/avatar.png",
+    image: "/userimg.jpeg",
     type: "website",
     ...customMeta,
   };
@@ -29,12 +29,12 @@ export default function ContainerBlock({ children, ...customMeta }) {
           href={`https://yourwebsite.com${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Abdullai Gubli" />
+        <meta property="og:site_name" content="GubliAS" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
+        <meta name="twitter:site" content="@GubliAS" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
